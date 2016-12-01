7d4ccd97596d53581233bb11ace61f53ec39a150
Adrian Cosma <adriancosma_en@yahoo.com>
2016-12-01 13:21:15 +0200

    initial commit

---
+++ src/main/java/com/functionaljpa/JpaFunctionalApplication.java
@@ -0,0 +1,12 @@
+package com.functionaljpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFunctionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaFunctionalApplication.class, args);
+	}
+}
